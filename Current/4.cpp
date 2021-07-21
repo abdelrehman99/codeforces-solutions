d9c86e432a9db61e4f2cc06e36d1288f06438b1b
abdelrehman99 <abelrehmanmamdouh@gmail.com>
2021-07-21 04:10:28 +0200

    k2

--- Current/4.cpp
+++ Current/4.cpp
@@ -12,28 +12,14 @@ ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans =
 
 
 
-void solve()
-{
-	int n, c = 0;
-    cin >> n;
-    vector <int> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            if (__gcd(v[i], v[j]) == 1) c++; 
-        }
-    }
-    if (c == 0) cout << "No estimate for this data set.\n";
-    else cout << sqrt ( ((n * 2) * 6) / (c * 1.0)) << endl;
+void solve(){
+	
 }
 
 
 
 
-signed main()
-{
+signed main(){
     fast;
     int t = 1;
     cin >> t;
