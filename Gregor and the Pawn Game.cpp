0eeb43846e4e65700c5e2007e179920d095a9835
abdelrehman99 <56692870+abdelrehman99@users.noreply.github.com>
2021-08-01 18:40:08 +0200

    Add files via upload

---
+++ Gregor and the Pawn Game.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC)
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+
+
+
+void solve()
+{ 
+    int n;
+    cin >> n;
+    char v[n + 1], v2[n + 1];
+    int c = 0;
+    for (int i = 1; i <= n; i++) cin >> v2[i];
+    for (int i = 1; i <= n; i++) cin >> v[i];
+    for (int i = 1; i <= n; i++)
+    {
+        if (v[i] == '0') continue;
+        if (v2[i] == '0')
+        {
+            v2[i] = '2';
+            c++;
+        }
+        else if (i - 1 >= 1 && (v2[i - 1] == '1') )
+        {
+            v2[i - 1] = '2';
+            c++;
+        }
+        else if (i + 1 <= n && (v2[i + 1] == '1') )
+        {
+            v2[i + 1] = '2';
+            c++;
+        }
+    }
+    cout << c << endl;
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    WL(t){
+    	solve();
+        runtime();
+    }
+    return 0;
+}
