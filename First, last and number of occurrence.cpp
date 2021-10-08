b271cbc0cc7ac546d6385100e5c8780e226896d5
abdelrehman99 <56692870+abdelrehman99@users.noreply.github.com>
2021-10-08 21:53:35 +0200

    Add files via upload

---
+++ First, last and number of occurrence.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define cin(v) for (auto &i : v) cin >> i;
+#define cout(v) for (auto &i : v) cout >> i;
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC)
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+struct num
+{
+    ll f, l, c = 0;
+};
+
+
+void solve()
+{ 
+    ll n, q, x;
+    cin >> n >> q;
+    map <ll, num> mp;
+    FOR(i, n)
+    {
+        cin >> x;
+        if (mp[x].c == 0) mp[x].f = i + 1, mp[x].l = i + 1, mp[x].c++;
+        else mp[x].l = i + 1, mp[x].c++;
+    }
+    FOR(i, q)
+    {
+        cin >> x;
+        if (mp[x].c == 0) cout << "-1\n";
+        else cout << mp[x].f << " " << mp[x].l << " " << mp[x].c << endl;
+    }
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    //cin >> t;
+    WL(t){
+    	solve();
+        runtime();
+    }
+    return 0;
+}
