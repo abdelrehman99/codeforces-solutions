56e5e61395da0199006aad0eeca57018a3a68f3a
abdelrehman99 <56692870+abdelrehman99@users.noreply.github.com>
2021-10-24 21:04:20 +0200

    Add files via upload

---
+++ Forming Teams.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 1; i <= (int) n; i++)
+#define cin(v) for (auto &i : v) cin >> i;
+#define cout(v) for (auto &i : v) cout >> i;
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+int n, m, cnt = 0;
+vvi v;
+vector <bool> vis;
+vector <int> color;
+
+void dfs (int i, int c)
+{
+    vis[i] = 1;
+    color[i] = c;
+    //if (v[i].empty()) return;
+    for (auto &j : v[i])
+    {
+        if (!vis[j]) dfs (j, !c);
+        else if (color[i] == color[j])
+        {
+            //cout << i << " " << j << endl;
+            cnt++;
+            return;
+        }
+    }
+}
+
+
+void solve()
+{
+    cin >> n >> m; 
+    int x, y;
+    v.resize(n + 1);
+    vis.resize(n + 1);
+    color.resize(n + 1, 0);
+    FOR (i, m)
+    {
+        cin >> x >> y;
+        v[x].PB(y);
+        v[y].PB(x);
+    }
+    FOR (i, n)
+    {
+        if (!vis[i]) dfs(i, 0);
+    }
+    cnt /= 2;
+    if ((n - cnt) % 2 != 0) cnt++;
+    cout << cnt << endl;
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    //cin >> t;
+    WL(t){
+    	solve();
+        runtime();
+    }
+    return 0;
+}
