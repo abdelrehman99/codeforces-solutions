992e89b2fc001f56f9d42b1cb9d4df8cb8329ea3
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2021-11-17 23:00:44 +0200

    Add files via upload

---
+++ Middle of the Contest.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define cin(v) for (auto &i : v) cin >> i;
+#define cout(v) for (auto &i : v) cout >> i;
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+
+
+
+void solve()
+{ 
+    int h1, m1, h2, m2;
+    char c;
+    cin >> h1 >> c >> m1;
+    //cout << h1 << " " << m1 << endl;
+    cin >> h2 >> c >> m2;
+    //cout << h2 << " " << m2 << endl;
+    int t1 = h1 * 60 + m1;
+	int t2 = h2 * 60 + m2;
+	int t3 = (t1 + t2) / 2;
+    if (t3 / 60 < 10) cout << "0";
+    cout << (t3 / 60);
+    cout << ":";
+    if (t3 % 60 < 10) cout << "0";
+    cout << t3 % 60;
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    //cin >> t;
+    WL(t){
+    	solve();
+    }
+    runtime();
+    return 0;
+}
