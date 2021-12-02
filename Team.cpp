fa1e25194a5edda911f368b715eb81d7d196f9ae
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2021-12-02 17:38:07 +0200

    Add files via upload

--- Team.cpp
+++ Team.cpp
@@ -1,23 +1,62 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	int n,c=0;
-	cin>>n;
-	int x[n][3];
-	for( int i = 0 ; i < n ; i++)
-	{
-		for( int j = 0 ; j < 3 ; j++)
-		{
-			cin>>x[i][j];
-		}
-	}
-	for( int i = 0 ; i < n ; i++)
-	{
-		if(x[i][0] + x[i][1] + x[i][2] >= 2)
-		{
-			c++;
-		}	
-	}
-	cout<<c;	
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define cin(v) for (auto &i : v) cin >> i;
+#define cout(v) for (auto &i : v) cout >> i;
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+
+
+
+void solve()
+{ 
+    ll n, k;
+    cin >> n >> k;
+    vl v(n);
+    FOR (i, n) cin >> v[i];
+    sort(all(v));
+    ll l = 0, r = n - 1, c = 0;
+    //map <ll, ll> mp;
+    while (l <= r)
+    {
+        if (v[r] >= k) c++, r--;
+        else if (v[l] >= k) c++, l++;
+        else if (v[l] + v[r] >= k && (l != r)) c++, l++, r--;
+        else l++;
+    }
+    cout << c << endl;
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    WL(t){
+    	solve();
+    }
+    runtime();
+    return 0;
 }
