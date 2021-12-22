0ca11b69a17a53d5ebfda0a61de5c1635c18bf2a
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2021-12-22 16:59:55 +0200

    Add files via upload

---
+++ Yet Another Array Restoration.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define cin(v) for (auto &i : v) cin >> i;
+#define cout(v) for (auto &i : v) cout >> i;
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+
+
+
+void solve()
+{ 
+    ll n, x, y;
+    cin >> n >> x >> y;
+    ll dif = y - x; 
+    vector<int> ans;
+    for (int d = 1; d <= y - x; ++d)
+    {
+        if ((y - x) % d != 0) continue;
+        vector<int> res;
+        bool foundx = false;
+        int cur = y;
+        int need = n;
+        while (cur >= 1 && need > 0) {
+            res.push_back(cur);
+            foundx |= cur == x;
+            --need;
+            cur -= d;
+        }
+        cur = y;
+        while (need > 0) {
+            cur += d;
+            res.push_back(cur);
+            --need;
+        }
+        sort(res.begin(), res.end());
+        if (need == 0 && foundx) {
+            if (ans.empty() || ans.back() > res.back()) {
+                ans = res;
+            }
+        }
+    }
+    for (auto it : ans) cout << it << " ";
+		cout << endl;
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    WL(t){
+    	solve();
+    }
+    runtime();
+    return 0;
+}
+
+/*
+for (ll i = y / n; i >= 1; i--)
+        {
+            if  (x % i == 0 && y % i == 0 && (y - ((n - 1) * i) <= x))
+            {
+                for (ll j = (y - ((n - 1) * i)); j <= y; j += i)
+                {
+                    cout << j << " ";
+                }
+                cout << endl;
+                return;
+            }
+        }
+*/
