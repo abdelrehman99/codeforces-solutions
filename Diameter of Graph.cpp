9be6c133be50657cbc9a5d64360de9bf72e45a64
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2022-02-11 02:22:51 +0200

    Add files via upload

---
+++ Diameter of Graph.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define cin(v) for (auto &i : v) cin >> i;
+#define cout(v) for (auto &i : v) cout >> i;
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+
+
+
+void solve()
+{ 
+    ll n, m, k;
+        cin >> n >> m >> k;
+        if (m < n - 1 || (m > n * (n - 1) / 2))
+        {
+            cout << "NO\n";
+            return;
+        }
+        ll mn_d = 2;
+        if (n == 1) mn_d = 0;
+        else if (n == 2) mn_d = 1;
+        else if (m == n * (n - 1) / 2) mn_d = 1;
+        cout << (mn_d < k - 1 ? "YES\n" : "NO\n");
+}
+
+
+
+
+signed main(){
+    fast;
+    int t = 1;
+    cin >> t;
+    WL(t){
+    	solve();
+    }
+    runtime();
+    return 0;
+}
