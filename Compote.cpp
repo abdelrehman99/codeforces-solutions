5e9e1c746ec1b832f69f05c7a300a67518189661
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2022-03-14 21:34:44 +0200

    Add files via upload

---
+++ Compote.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define F first
+#define FOR(i, n) for (int i = 0; i < (int)n; i++)
+#define cin(v, n)                    \
+    for (int i = 0; i < (int)n; i++) \
+        cin >> v[i];
+#define cout(v, n)                   \
+    for (int i = 0; i < (int)n; i++) \
+        cout << v[i] << (i == n - 1 ? "\n" : " ");
+#define WL(t) while (t--)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+
+void solve()
+{
+    ll l, a, p, x = 1, y = 2, z = 4, c = 1;
+    cin >> l >> a >> p;
+    while (true)
+    {
+        if ((x * c <= l) && (y * c <= a) && (z * c <= p)) c++;
+        else
+        {
+            c--;
+            break;
+        }
+    }
+    //cout << c << endl;
+    cout << (x + y + z) * c << endl;
+}
+
+signed main()
+{
+    fast;
+    int t = 1;
+    //cin >> t;
+    WL(t)
+    {
+        solve();
+    }
+    runtime();
+    return 0;
+}
