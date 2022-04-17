ed52e1d6b706c2c43b22ea6de83e692db9e0a96d
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2022-04-17 22:09:03 +0200

    Add files via upload

---
+++ Red Versus Blue.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define F first
+#define FOR(i, n) for (int i = 0; i < (int)n; i++)
+#define cin(v, n)                    \
+    for (int i = 0; i < (int)n; i++) \
+        cin >> v[i];
+#define cout(v, n)                   \
+    for (int i = 0; i < (int)n; i++) \
+        cout << v[i] << (i == n - 1 ? "\n" : " ");
+#define WL(t) while (t--)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+#define ret return
+#define endl "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+
+void solve()
+{
+    ll n, r, b;
+    cin >> n >> r >> b;
+    string s;
+    ll count = ((n) / (b + 1));
+    for (ll i = 1; b > 0; i++)
+    {
+        for (ll j = 1; j <= count && r > 0; j++, r--)
+            s.PB('R');
+        if (b > 0 && r == 0)
+        {
+            break;
+        }
+        else
+            s.PB('B'), b--;
+    }
+    for (ll j = 1; r > 0; j++, r--)
+        s.PB('R');
+    for (auto i : s)
+    {
+        if (i == 'R' && b > 0)
+        {
+            cout << 'B';
+            b--;
+        }
+        cout << i; 
+    }
+    cout << endl;
+}
+
+signed main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    WL(t)
+    {
+        solve();
+    }
+    runtime();
+    return 0;
+}
