d63b8240ad7438845366fad6cd24dd6281a45fd1
Abdelrehman Mamdouh Wahid <56692870+abdelrehman99@users.noreply.github.com>
2022-04-26 13:10:54 +0200

    Add files via upload

---
+++ Replace and Keep Sorted.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define F first
+#define FOR(i, n) for (int i = 0; i < (int)n; i++)
+#define cin(v, n)                    \
+    for (int i = 0; i < (int)n; i++) \
+        cin >> v[i];
+#define cout(v, n)                   \
+    for (int i = 0; i < (int)n; i++) \
+        cout << v[i] << (i == n - 1 ? "\n" : " ");
+#define WL(t) while (t--)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+#define MP make_pair
+#define llu unsigned long long
+#define ret return
+#define endl "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+
+void solve()
+{
+    ll n, q, k;
+    cin >> n >> q >> k;
+    vl v(n), v2(n);
+    cin (v, n);
+    v2[0] = (v[1] - 2);
+    for (ll i = 1; i < n - 1; i++)
+    {
+        v2[i] = v[i + 1] - v[i - 1] - 2 + v2[i - 1];
+    }
+    while (q--)
+    {
+        ll l, r;
+        cin >> l >> r;
+        l--, r--;
+        if (l == r)
+        {
+            cout << k - 1 << endl;
+            continue;
+        }
+        ll sum = (v[l + 1] - 2) + (k - v[r - 1] - 1) + (v2[r - 1] - v2[l]);
+        cout << sum << endl;
+    }
+}
+
+signed main()
+{
+    fast;
+    int t = 1;
+    //cin >> t;
+    WL(t)
+    {
+        solve();
+    }
+    runtime();
+    return 0;
+}
